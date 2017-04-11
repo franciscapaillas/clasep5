86257527101b934a7f87fa24f37c3ac4d2f86946
franciscapaillas <fran.paillas@gmail.com>
2017-04-11 10:44:27 -0300

    Update uno.js

--- p5/uno.js
+++ p5/uno.js
@@ -1,16 +1,11 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight/2);
-  background(0);
-  noLoop();
+  createCanvas(windowWidth/2,windowHeight/2);
+  background(200);
 }
 
 function draw() {
-  for (var x = 0; x < windowWidth; x+=windowWidth/25) {
-    colorMode(HSB);
-    noStroke();
-    fill(200,x/15,255);
-    ellipseMode(CENTER);
-
-    ellipse(x, windowHeight/4, 50, 50);
-  }
+  rectMode(CENTER);
+  rect(width/2, height/2, 50, 50);
+  triangle(30, 75, 58, 20, 86, 75);
+  line(mouseX, 0, mouseX, 400);
 }
